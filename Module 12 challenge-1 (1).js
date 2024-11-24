12c589c984b43868f5cb402e8c3006a63ff60f00
Draca Baugh-Flowers <66185683+Gradius64@users.noreply.github.com>
2024-11-24 13:48:59 -0600

    Add files via upload

---
+++ Module 12 challenge-1 (1).js
@@ -0,0 +1,30 @@
+
+import './Nav.css';
+import'./Navigation';
+
+const Header = () => {
+ 
+
+
+  
+
+const headerStyle =  {
+    backgroundColor: '#4CAF50',
+    color: 'white',
+    padding: '10px 20px',
+    textAlign: 'center'
+  
+    
+
+
+
+  
+
+/// Navigation component
+
+
+
+  
+
+}
+}
